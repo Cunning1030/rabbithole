c5bd1c799e86cc003310025e3a40047bbafc07e5
Benjamin <cunningham1030@yahoo.com>
2018-08-06 17:31:26 +0200

    1.3 Styled post and contact forms. Styled home and post index views. Added featured_post and most_recent_post methods to main_pages controller to be utilized in index view. Added some sample images. Added new/edit post link to nav. Added sample content to about view.

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,6 +1,8 @@
 class MainPagesController < ApplicationController
 
   def index
+    @featured_post = Post.offset(rand(Post.count)).first
+    @most_recent_post = Post.order('created_at desc').first
   end
 
 end
